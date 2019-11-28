f0701602d7e8386a39f652e8c86bde37cc1d08aa
Niko Hienonen <niko.hienonen@gmail.com>
2019-11-28 19:23:01 +0200

    Screens created
    CommitScreen only shows the name of the user, reposcreen just navigates to commitscreen.

--- components/CommitScreen.js
+++ components/CommitScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class CommitScreen extends Component {
   static navigationOptions = ({navigation}) => {
@@ -13,7 +13,20 @@ export default class CommitScreen extends Component {
   }
   render() {
     return (
-    <Text>{this.props.navigation.state.params.name}</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>{this.props.navigation.state.params.name}'s repositories</Text>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    margin: 10
+  },
+  title: {
+    fontSize: 30
+  }
+})
--- components/RepoScreen.js
+++ components/RepoScreen.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 export default class RepoScreen extends Component {
   static navigationOptions = {
-    title: 'Repos',
+    title: 'Repo Viewer App',
   };
+  state = {
+    inputValue: ""
+  }
+  onChangeText = text => {
+    this.setState({inputValue: text});
+  }
   render() {
     const {navigate} = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Repo Viewer App</Text>
-        <Button
-          title="Go to Jane's profile"
-          onPress={() => navigate('Commits', {name: 'Jane'})}
-        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => this.onChangeText(text)}
+            placeholder="Search for user..."
+          />
+          <Button
+            title="Search"
+            onPress={() => navigate('Commits', {name: 'Vincit'})}
+            style={styles.button}
+          />
+        </View>
+        <View style={styles.list}></View>
       </View>
     );
   }
@@ -22,10 +36,22 @@ export default class RepoScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    marginTop: 50,
+    alignItems: 'stretch',
+    margin: 10
   },
-  title: {
-    fontSize: 30
-  }
+  inputContainer: {
+    flexDirection:'row', 
+  },
+  input: {
+    flex : 1,
+    borderColor: 'gray', 
+    borderWidth: 1,
+    paddingLeft: 5,
+    lineHeight: 20,
+  },
+  button: {
+  },
+  list: {
+    backgroundColor: 'maroon'
+  },
 })
